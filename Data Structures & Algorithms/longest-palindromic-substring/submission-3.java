03d946cfa34449c17c0b27c292a389a9b0ea9ee2
GGxAhem <103478589+GGxAhem@users.noreply.github.com>
2026-08-01 11:13:08 +0530

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        String pal = s.substring(0, 1);
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // Odd length palindrome
+            int low = i;
+            int high = i;
+
+            while (low >= 0 && high < s.length() &&
+                    s.charAt(low) == s.charAt(high)) {
+                low--;
+                high++;
+            }
+
+            String palindrome = s.substring(low + 1, high);
+            if (palindrome.length() > pal.length()) {
+                pal = palindrome;
+            }
+
+            // Even length palindrome
+            low = i;
+            high = i + 1;
+
+            while (low >= 0 && high < s.length() &&
+                    s.charAt(low) == s.charAt(high)) {
+                low--;
+                high++;
+            }
+
+            palindrome = s.substring(low + 1, high);
+            if (palindrome.length() > pal.length()) {
+                pal = palindrome;
+            }
+        }
+
+        return pal;
+    }
+}
